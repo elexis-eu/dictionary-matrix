b5368cab5745187b0eff899ba38337864c27789c
Kernc <kerncece@gmail.com>
2021-02-24 01:25:04 +0100

    Fix scripts/tei2ontolex.py stdout output

--- scripts/tei2ontolex.py
+++ scripts/tei2ontolex.py
@@ -18,7 +18,7 @@ def convert(input: str, output: str = None):
 
     xml = ET.parse(input)
     xslt = ET.XSLT(ET.parse(XSLT_PATH), access_control=ET.XSLTAccessControl.DENY_ALL)
-    output_fd = sys.stdout if output == '-' else open(output, 'wb')
+    output_fd = sys.stdout.buffer if output == '-' else open(output, 'wb')
     output_fd.write(ET.tostring(xslt(xml)))
     print('Written to', output, file=sys.stderr)
     print(xslt.error_log, file=sys.stderr)
